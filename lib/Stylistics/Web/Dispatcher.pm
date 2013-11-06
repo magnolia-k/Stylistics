4884dd51897607989f0524d6fdbf3b38af90397d
magnolia <magnolia.k@me.com>
2013-11-06 21:54:01 +0900

    Add previous article, and next article.

--- lib/Stylistics/Web/Dispatcher.pm
+++ lib/Stylistics/Web/Dispatcher.pm
@@ -28,13 +28,19 @@ any '/' => sub {
 any '/blog/' => sub {
 	my ( $c ) = @_;
 
-	return $c->render( 'blog.tx' => { entries => $blog->list( limit => undef ) } );
+	return $c->render( 'blog.tx' => {
+			entries => $blog->list( limit => undef ),
+			title   => 'Blog | code-stylistics.net',
+			} );
 };
 
 any '/archives/' => sub {
 	my ( $c ) = @_;
 
-	return $c->render( 'archives.tx' => { entries => $blog->list( limit => 10 ) } );
+	return $c->render( 'archives.tx' => {
+			title   => 'Blog Archives | code-stylistics.net',
+			entries => $blog->list( limit => 10 )
+			} );
 };
 
 any 'archives/{article_index}' => sub {
@@ -45,15 +51,37 @@ any 'archives/{article_index}' => sub {
 
 	my $article = $blog->article( entry_id => $entry_id );
 
-	return $c->render( 'article.tx' => { entry => $article } );
+	my $previous = $blog->previous_article( entry_id => $entry_id);
+	my $previous_link = $previous ? $previous->filename : undef;
+
+	my $previous_link_html = $previous_link ?
+		qq{<li><a href="./$previous_link">Previous</a></li>} :
+		undef;
+
+	my $next     = $blog->next_article( entry_id => $entry_id);
+	my $next_link = $next ? $next->filename : undef;
+
+	my $next_link_html = $next_link ?
+		qq{<li><a href="./$next_link">Next</a></li>} :
+		undef;
+
+	return $c->render( 'article.tx' => {
+			entry => $article,
+			title => $article->title . ' | code-stylistics.net',
+			previous_link => $previous_link_html,
+			next_link     => $next_link_html,
+		   	} );
 };
 
 any '/enbld/' => sub {
 	my ( $c ) = @_;
 
-	return $c->render( 'enbld.tx' );
+	return $c->render( 'enbld.tx' => {
+			title => 'Enbld | code-stylistics.net'
+			} );
 };
 
+# Now disabled about menu.
 any '/about/' => sub {
 	my ( $c ) = @_;
 
---
+++ static/blog/perlbeginners11.md
@@ -0,0 +1,12 @@
+11月29日に開催されるPerl Beginners #11というイベントで基調講演をやることになりました。
+
+→[Perl Beginners](http://www.perl-beginners.org)
+
+- magnolia: Perl-Beginnersに参加したら、音速でCPAN Authorになった話
+
+これまでperl界隈の人たちとの交流が全然無かったのですが、ふと思い立ってPerl Beginners #10で飛び込みLTをさせてもらったことをきっかけに、[Enbld](http://search.cpan.org/dist/Enbld/)を（名前も変えて、ドキュメントも大幅に書き加えて…）、CPANにアップするところまで来ました。
+
+Perl Beginners #11のテーマは「スピードアップ」という事なので、1ヶ月の間にやった色んな事を纏めて話したいと思います。
+
+なお、参加される方は[ATND](http://atnd.org/events/45001)から登録して下さい。
+
